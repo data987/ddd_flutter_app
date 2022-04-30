d8b13cd479559e60af31552ec5e3c752f9f52d92
Daniel Tavera <dataanm@gmail.com>
2022-04-30 13:35:01 -0500

    Adding domain auth failure files

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:ddd_flutter_app/domain/auth/value_object.dart';
+
+import 'auth_failure.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
